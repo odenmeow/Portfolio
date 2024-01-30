8e525079737292f6d27627352c610f41ef6412d0
Oni <qw28425382694@gmail.com>
2024-01-31 01:33:17 +0800

    改跟著Next的deploy做看看

--- my-next/next.config.js
+++ my-next/next.config.js
@@ -5,20 +5,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  distDir: "out",
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      "/": { page: "/" }, // 对应主页
-      "/404": { page: "/404" }, // 404 页面
-      // 其他页面...
-
-      // 使用默认导出路径映射
-      ...defaultPathMap,
-    };
-  },
+  output: "export",
 };
 
 module.exports = nextConfig;
